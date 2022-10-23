a8aa396c693672b294e15619efd3b4a7a0f5b762
obithree <projectofsavepiece0706@gmail.com>
2022-10-23 15:28:44 +0900

    feat: テスト用のアプリケーションたたき台

---
+++ web_app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello world!</p>"
+
+@app.route("/test")
+def exec_test():
+    return "<p>TestTest!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
